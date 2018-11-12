ec7410ab54efefd87b297947892727cfb5309fd0
Yener Yardim <yyardim@gmail.com>
2018-11-12 00:13:08 -0800

    material added to the proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'TartisiYorum';
+  title = 'TartisiYorum.com is back on BlockChain!';
 }
